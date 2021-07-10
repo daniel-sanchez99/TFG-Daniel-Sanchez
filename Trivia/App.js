889d3168bc314b223ef0889a205b999dfb52a2cc
Daniel <dsd218@alumnos.unican.es>
2021-07-10 20:54:46 +0200

    Ya se hace bien la peticion a la API y llegan preguntas (ver en la consola de debug). Todavia hay codigo de prueba.

--- Trivia/App.js
+++ Trivia/App.js
@@ -1,150 +1,76 @@
-/* eslint-disable react-native/no-inline-styles */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Button, Text } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Clicker from './src/components/Clicker';
+import Preguntas from './src/components/PreguntasTest';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-const MyStack = () => {
+const MyStack = (): Node => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={App}
-          options={{ title: 'Welcome' }}
+          component={Home}
+          options={{
+            headerStyle: { backgroundColor: 'rgb(74, 74, 74)' },
+            headerTintColor: 'white',
+          }}
         />
-        <Stack.Screen name="HolaMundo" component={HolaMundo} />
+        <Stack.Screen
+          name="Clicker"
+          component={Clicker}
+          options={{
+            headerStyle: { backgroundColor: 'rgb(74, 74, 74)' },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen
+          name="PreguntasTest"
+          component={Preguntas}
+          options={{
+            headerStyle: { backgroundColor: 'rgb(74, 74, 74)' },
+            headerTintColor: 'white',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const Section = ({ children, title, navigation }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Home = ({ navigation }): Node => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const HolaMundo = ({ navigation }) => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
-  );
-};
-
-const App: () => Node = ({ navigation }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More My panaaaaa">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-          <Button
-            title="Go to Jane's profile"
-            onPress={() => navigation.navigate('HolaMundo')}
-          />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texto}> TRIVIA </Text>
+      <Button
+        title="Ir a clicker"
+        onPress={() => navigation.navigate('Clicker')}
+      />
+      <Button
+        title="Comenzar"
+        onPress={() => navigation.navigate('PreguntasTest')}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'rgb(46, 46, 46)',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  texto: {
+    fontWeight: 'bold',
+    marginTop: 30,
+    color: 'white',
+    fontSize: 40,
   },
 });
 
